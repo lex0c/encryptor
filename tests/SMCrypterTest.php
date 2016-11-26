35aaf25e7ccc31bf5e9dd8b656aee7fe6e1b8bf9
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-26 15:11:19 -0200

    Tests for integrity

--- tests/SMCrypterTest.php
+++ tests/SMCrypterTest.php
@@ -5,8 +5,15 @@ require_once (__DIR__ . '/../encryption/src/SMCrypter.php');
 $smCrypt1 = new SMCrypter();
 $smCrypt2 = new SMCrypter();
 
-$originalValue = "Meu EMAIL é 'leonardo_carvalho@outlook.com' and (10+5/2*7=0999) <>because yes. çÇ>";
-echo 'Original Text: '.$originalValue;
+$phase = "Meu EMAIL é 'leonardo_carvalho@outlook.com' and (10+5/2*7=0999) <><because yes. çÇ>";
+$alphabet = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, Ç';
+$especials = '! @ # $ % ¨ & * ( ) _ + = , . ; / ? | \\ ° ° : ^ ~ ] } { [ ´ ` º ª';
+$accentuation = 'í ,ó, é,á, ú, õ, ẽ, ã, ẽ, ũ, ñ, â, ô';
+$numbers = '1 2 3 4 5 6 7 8 9 0';
+$tag = '<?php echo "hello!" ?>';
+
+$originalValue = $phase;
+echo 'Original Text: ' . htmlentities($originalValue);
 
 echo "<br>";
 echo "<br>";
@@ -14,7 +21,7 @@ echo "<br>";
 $key = $smCrypt1->keyGenerator();
 //$key = 3000000;
 //$key = 'VE16Z2pNPT1RTzVn';
-echo 'Generated Key: '.$key;
+echo 'Generated Key: ' . $key;
 
 echo "<br>";
 echo "<br>";
