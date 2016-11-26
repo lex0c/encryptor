25340822fbc033606eacd6c45058e0b7876338ab
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-26 14:41:56 -0200

    Tests for encryptation

---
+++ tests/DisguiseTest.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once (__DIR__ . '/../src/Disguise.php');
+
+var_dump(Disguise::illumin(Disguise::obscure('Testando hoje pq simmm')));
---
+++ tests/SMCrypterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once (__DIR__ . '/../encryption/src/SMCrypter.php');
+
+$smCrypt1 = new SMCrypter();
+$smCrypt2 = new SMCrypter();
+
+$originalValue = "Meu EMAIL é 'leonardo_carvalho@outlook.com' and (10+5/2*7=0999) <>because yes. çÇ>";
+echo 'Original Text: '.$originalValue;
+
+echo "<br>";
+echo "<br>";
+
+$key = $smCrypt1->keyGenerator();
+//$key = 3000000;
+//$key = 'VE16Z2pNPT1RTzVn';
+echo 'Generated Key: '.$key;
+
+echo "<br>";
+echo "<br>";
+
+$en = $smCrypt1->encode($key, $originalValue);
+echo 'Encrypted Text: '.$en;
+
+echo "<br>";
+echo "<br>";
+
+echo 'Decrypted Text: '.$smCrypt2->decode($key, $en);
