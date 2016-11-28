1a26b0384d96d01133cf27b4a48c5c9af02c23e6
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-28 16:12:37 -0200

    Tests with PHPUnit

---
+++ tests/phpunit/HashGeneratorTest.php
@@ -0,0 +1,12 @@
+<?php namespace Tests\PHPUnit;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+use \Encryptor\Suite\HashGenerator;
+
+/**
+ *  
+ */
+final class HashGeneratorTest extends TestCase
+{
+    
+}
---
+++ tests/phpunit/SMCrypterTest.php
@@ -0,0 +1,12 @@
+<?php namespace Tests\PHPUnit;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+use \Encryptor\Suite\SMCrypter;
+
+/**
+ *  
+ */
+final class SMCrypterTest extends TestCase
+{
+    
+}
